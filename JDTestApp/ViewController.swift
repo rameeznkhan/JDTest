80ea2f8085fa404526ffa15fb93c5b5e95f91097
rameez <rameeznkhan@gmail.com>
2019-06-15 14:13:39 +0530

    Initial Commit

---
+++ JDTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JDTestApp
+//
+//  Created by Rameez khan  on 15/06/19.
+//  Copyright © 2019 Rameez khan . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
